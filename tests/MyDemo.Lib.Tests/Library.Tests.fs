1ce89ca1097f20d18b450966769d6fd7a0740911
Patrick Drechsler <patrick@pdrechsler.de>
2023-09-14 21:31:43 +0200

    ⚗️ Experiment with VerifySettings & DiffTools
    - Store snapshots in dedicated folder
    - Disable DiffTools and use Rider's Verify plugin

--- tests/MyDemo.Lib.Tests/Library.Tests.fs
+++ tests/MyDemo.Lib.Tests/Library.Tests.fs
@@ -3,8 +3,26 @@ module Tests
 
 open Xunit
     
-[<Fact>]
-let ``Working with Verify - Example 1`` () =
+[<Fact(Skip = "Ignore, because the verified file is placed next to the code")>]
+let ``Example 1`` () =
     let simpleMap = Map [1, "a"; 2, "b"]
-    let newMap = Map.add 3 "c" simpleMap
-    Verifier.verify(newMap)
+    Verifier.verify simpleMap
+
+[<Fact>]
+let ``Example 2 using a custom output folder`` () =
+    let simpleMap = Map [1, "a"; 2, "b"]
+    Verifier.verify' Verifier.verifySettings simpleMap
+
+let verify value =
+    Verifier.verify' Verifier.verifySettings value
+
+[<Fact>]
+let ``Example 3 another wrapper`` () =
+    let simpleMap = Map [1, "a"; 2, "b"]
+    verify simpleMap
+
+[<Fact>]
+let ``Example 4 diff tool`` () =
+    Verifier.initializeDiffTool ()
+    let simpleMap = Map [1, "a"; 2, "b1"]
+    verify simpleMap
--- tests/MyDemo.Lib.Tests/Verifier.fs
+++ tests/MyDemo.Lib.Tests/Verifier.fs
@@ -3,7 +3,9 @@ module Verifier
 
 // Solution from Urs: https://tooting.ch/@UrsEnzler/111063884182280956
 
+open Argon
 open System.Threading.Tasks
+open DiffEngine
 open VerifyTests
 open VerifyXunit
 
@@ -18,3 +20,21 @@ let inline verifyUsingParameters parameters (value: 't :> obj) =
     
 let inline verifyUsingParameters' parameters (settings: VerifySettings) (value: 't :> obj) =
     Verifier.Verify(value :> obj, settings).UseParameters(parameters).ToTask() :> Task
+
+let verifySettings =
+    let settings = VerifySettings ()
+    settings.UseDirectory "snapshots"
+    settings.AddExtraSettings(fun s -> s.NullValueHandling <- NullValueHandling.Include)
+    settings
+
+let initializeDiffTool () =
+    // DiffTools.UseOrder(DiffTool.Rider) // This has no effect when used with Rider and Linux?
+    // DiffTools.UseOrder(DiffTool.Meld) // Seems to be the default on my system?
+    
+    // Use Rider plugin `Verify` by right-clicking in the test explorer and selecting
+    // 
+    //  "Compare Received/Verified"
+    // or
+    //  "Accept Received"
+    //
+    DiffRunner.Disabled <- true
