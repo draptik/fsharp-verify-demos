a5828549fd0e3ff3be9b296ca001a59680bb8d2e
Patrick Drechsler <patrick@pdrechsler.de>
2023-09-16 20:29:07 +0200

    🧪 Move bad-practice tests to dedicated module 'DontDoThis'

--- tests/MyDemo.Lib.Tests/Library.Tests.fs
+++ tests/MyDemo.Lib.Tests/Library.Tests.fs
@@ -26,23 +26,31 @@ let ``Example 4 diff tool`` () =
     let simpleMap = Map [1, "a"; 2, "b1"]
     Verifier.verify simpleMap
 
-[<Fact>]
-let ``SimpleName is verifiable`` () =
-    let sut = { SimpleName.FirstName = "Homer"; LastName = "Simpson" }
-    Verifier.verify sut
+/// Verify shouldn't be used for checking the internal data structure anyway.
+/// The whole point of Verify (and similar Golden-Master testing tools) is to
+/// test the result at the boundary of the application (i.e. the public API).
+/// 
+/// So testing F# specific types is not a good idea.
+[<VerifyXunit.UsesVerify>]
+module DontDoThis =
 
-[<Fact(Skip = "This fails: An F# option type isn't serialized correctly by default")>]
-let ``SimpleNameOption is verifiable when value is present`` () =
-    let sut = { SimpleNameOption.FirstName = Some "Homer"; LastName = "Simpson" }
-    Verifier.verify sut
+    [<Fact>]
+    let ``SimpleName is verifiable`` () =
+        let sut = { SimpleName.FirstName = "Homer"; LastName = "Simpson" }
+        Verifier.verify sut
 
-[<Fact(Skip = "This fails: An F# option type isn't serialized correctly by default")>]
-let ``SimpleNameOption is verifiable when value is missing`` () =
-    let sut = { SimpleNameOption.FirstName = None; LastName = "Simpson" }
-    Verifier.verify sut
+    [<Fact(Skip = "This fails: An F# option type isn't serialized correctly by default")>]
+    let ``SimpleNameOption is verifiable when value is present`` () =
+        let sut = { SimpleNameOption.FirstName = Some "Homer"; LastName = "Simpson" }
+        Verifier.verify sut
+
+    [<Fact(Skip = "This fails: An F# option type isn't serialized correctly by default")>]
+    let ``SimpleNameOption is verifiable when value is missing`` () =
+        let sut = { SimpleNameOption.FirstName = None; LastName = "Simpson" }
+        Verifier.verify sut
 
 
-[<Fact(Skip = "This fails: An F# discriminated union isn't serialized correctly by default")>]
-let ``PaymentMethod is verifiable when value is CreditCard`` () =
-    let sut = CreditCard { CardNumber = "123"; ExpiryDate = new DateTime(2019, 1, 1) }
-    Verifier.verify sut
+    [<Fact(Skip = "This fails: An F# discriminated union isn't serialized correctly by default")>]
+    let ``PaymentMethod is verifiable when value is CreditCard`` () =
+        let sut = CreditCard { CardNumber = "123"; ExpiryDate = new DateTime(2019, 1, 1) }
+        Verifier.verify sut
